d172713c1b4aa84336b90d5e125e4405f72f0a4c
ShamilAzaxov <shamil19970809@gmail.com>
2021-10-26 17:36:52 +0400

    My first one

---
+++ WorkingWithGit/src/working_with_git/Main.java
@@ -0,0 +1,9 @@
+package working_with_git;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("bla bla");
+        System.out.println("dmfxnvnknvkjd");
+    }
+    
+}
